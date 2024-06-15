feb98da814af7dbc67f6a0de5cf24b2359967a73
Bahaa Youssof <bahaayoussof@gmail.com>
2024-06-15 10:02:15 +0300

    use json utility

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
@@ -13,11 +13,13 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw new Response(
-      JSON.stringify(JSON.stringify({ message: "Failed to fetch events." }), {
-        status: 500,
-      })
-    );
+    // throw new Response(
+    //   JSON.stringify(JSON.stringify({ message: "Failed to fetch events." }), {
+    //     status: 500,
+    //   })
+    // );
+
+    throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     const resData = await response.json();
 
